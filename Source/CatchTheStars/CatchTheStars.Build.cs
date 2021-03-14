a7400ac40466c01f0d8e5897dabf1ced26f5a4ce
andrescrd <c.andresricardo@gmail.com>
2021-03-13 21:14:54 -0300

    removing unused modules

--- Source/CatchTheStars/CatchTheStars.Build.cs
+++ Source/CatchTheStars/CatchTheStars.Build.cs
@@ -8,12 +8,12 @@ public class CatchTheStars : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","NavigationSystem", "AIModule", "Niagara", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","Niagara", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
-		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
